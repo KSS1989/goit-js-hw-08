54dc0d6d534e46c80c2ba639bf3c4fdf338c8094
Serhii Klokun <99805859+KSS1989@users.noreply.github.com>
2023-03-11 22:29:47 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,32 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const item = [];
+
+galleryItems.forEach((el) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = el.original;
+  const galleryImg = document.createElement("img");
+  galleryImg.className = "gallery__image";
+  galleryImg.src = el.preview;
+  galleryImg.setAttribute("data-source", el.original);
+  galleryImg.alt = el.description;
+
+  galleryLink.append(galleryImg);
+  item.push(galleryLink);
+});
+gallery.append(...item);
+
+new SimpleLightbox('.gallery__link');
