b044a372d2d923d878ce7bb73ba88f62e06652df
Serhii Klokun <99805859+KSS1989@users.noreply.github.com>
2023-03-14 00:12:58 +0200

    add feedback

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
 
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import SimpleLightbox from "simplelightbox";
-
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 const item = [];
 
-galleryItems.forEach((el) => {
-  const galleryLink = document.createElement("a");
-  galleryLink.className = "gallery__link";
-  galleryLink.href = el.original;
-  const galleryImg = document.createElement("img");
-  galleryImg.className = "gallery__image";
-  galleryImg.src = el.preview;
-  galleryImg.setAttribute("data-source", el.original);
-  galleryImg.alt = el.description;
+galleryItems.forEach(el => {
+  const galleryLink = document.createElement('a');
+  galleryLink.className = 'gallery__link';
+  galleryLink.href = el.original;
+  const galleryImg = document.createElement('img');
+  galleryImg.className = 'gallery__image';
+  galleryImg.src = el.preview;
+  galleryImg.setAttribute('data-source', el.original);
+  galleryImg.alt = el.description;
 
-  
-  galleryLink.append(galleryImg);
-  item.push(galleryLink);
+  galleryLink.append(galleryImg);
+  item.push(galleryLink);
 });
 gallery.append(...item);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener(
+  'input',
+  throttle(e => {
+    dataSaveText();
+  }, 500)
+);
+form.addEventListener('submit', onSubmit);
+
+function dataSaveText() {
+  const dataSave = { email: email.value, message: message.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataSave));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+const loadData = key => {
+  const serializedState = localStorage.getItem(key);
+  return serializedState === null ? undefined : JSON.parse(serializedState);
+};
+
+const storageData = loadData(LOCALSTORAGE_KEY);
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+}
